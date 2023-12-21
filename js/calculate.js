2fbbbeedcd3bdbfa22e1d197dd532c2d8066ffa9
Masudur Rahman <115950428+masudpatwary@users.noreply.github.com>
2023-12-22 00:52:06 +0600

    update

--- js/calculate.js
+++ js/calculate.js
@@ -11,6 +11,14 @@ document.getElementById("bg-color-1").addEventListener("mouseenter", () => {
     randomRGBColor();
 });
 
+// Calculate Variable 
+
+const calculateResult = `<div class="text-xl flex justify-between items-center">
+                        <p id="header-name"></p>
+                        <p> <span id="calculate-value">12</span>cm <sup>2</sup></p>
+                        <button class="bg-[#1090d8] py-2 px-3 rounded-md text-white font-semibold">Covert to m <sup>2</sup></button>
+                    </div> `;
+
 // Thir is Globle Scope
 
 const a = 0.5;
@@ -41,4 +49,14 @@ function getCalculate() {
 
 document.getElementById("calculate-btn-1").addEventListener("click", () => {
   getCalculate();
+  const parent = document.getElementById("result-parent");
+  parent.innerHTML = calculateResult;
 });
+
+
+
+// document.getElementById("add-btn").addEventListener("click", () => {
+//   const parent = document.getElementById("result-parent");
+//   parent.innerHTML = calculateResult;
+// });
+
